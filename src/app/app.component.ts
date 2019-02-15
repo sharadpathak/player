7da34ac0eba8bb063877b49da8a1bd5415ab37cb
Sharad Pathak <shapatha@publicisgroupe.net>
2019-02-15 17:20:34 +0530

    DEmo Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+// Declara a lib do videojs como externa ao angular
+declare let videojs: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'simple-player';
+   // Title of component
+   title = 'Player';
+   // Instance of video.js.
+   vidObj: any;
+   // Poster for no video.js
+   poster = '//d2zihajmogu5jn.cloudfront.net/elephantsdream/poster.png';
+   // URL of video.
+   videoUrl = '//d2zihajmogu5jn.cloudfront.net/elephantsdream/ed_hd.mp4';
+   
+   @ViewChild('myvid') vid: ElementRef;
+ 
+   ngAfterViewInit() {
+     const options = {
+       controls: true,
+       autoplay: false,
+       loop: true,
+       preload: 'auto',
+       techOrder: ['html5']
+     };
+ 
+     this.vidObj = new videojs(this.vid.nativeElement, options, function onPlayerReady() {
+       videojs.log(videojs(this.vid.nativeElement));
+     }); 
+   
+     console.log(new videojs(this.vid.nativeElement))
+   }
 }
